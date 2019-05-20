e92c8318608487ce6a33a93ff43fa08c4da34ac9
Negin Firouzian <negfir@gmail.com>
2019-05-20 19:39:23 +0430

    jQuery Done!

--- JS/login.js
+++ JS/login.js
@@ -1,14 +1,28 @@
 function loginClickL()
 {
-$( ".middle" ).load("../src/login.html");
-//$( ".middle" ).css(z-index:9)
+$("#MyCSS").attr("href","../assets/login.css"); 
+$( ".top" ).load("../src/login.html .top"); 
+$( ".middle" ).load("../src/login.html  .middle");
 }
 
 function loginClickR()
 {
-	var mid;
-	mid=load("../src/register.html");
-$( ".middle" ).load("../src/register.html");
-//$( ".middle" ).css(z-index:9)
+$("#MyCSS").attr("href","../assets/register.css"); 
+$( ".top" ).load("../src/register.html .top"); 
+$( ".middle" ).load("../src/register.html .middle");
+}
+
+function registerClickL()
+{
+$("#MyCSS").attr("href","../assets/login.css"); 
+$( ".top" ).load("../src/login.html .top"); 
+$( ".middle" ).load("../src/login.html  .middle");
+}
+
+function registerClickR()
+{
+$("#MyCSS").attr("href","../assets/register.css"); 
+$( ".top" ).load("../src/register.html .top"); 
+$( ".middle" ).load("../src/register.html .middle");
 }
 
