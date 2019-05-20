25db917c9b8a21a651c1a0dc7e9af9e3d7428e30
Negin Firouzian <negfir@gmail.com>
2019-05-20 16:43:50 +0430

    jQuery added

---
+++ JS/login.js
@@ -0,0 +1,19 @@
+
+
+
+
+function loginClickL()
+{
+$( ".middle" ).load("../src/login.html");
+//$( ".middle" ).css(z-index:9)
+}
+
+function loginClickR()
+{
+	var mid;
+	mid=load("../src/register.html");
+$( ".middle" ).load("../src/register.html");
+//$( ".middle" ).css(z-index:9)
+}
+
+
